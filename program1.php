ad444ed72b94e7e8528fb4e00cd6fb9bc5c6e8b8
Normaneil <normaneil.macutay@gmail.com>
2018-11-26 17:14:31 +0800

    Initial commit
    Write the following program in PHP where:
    example input =  "HEELLLOLAA"
    output = "H1E2L3O1L1A2"

---
+++ program1.php
@@ -0,0 +1,45 @@
+<?php
+$string= 'HELLO';
+echo "{$string}<hr>";
+
+// Get first character
+$temp= substr($string, 0, 1);
+$letters= [];
+
+// Fun stuff starts here
+// Code hack
+// Need to add one character position in the last of the string
+// This will enable the script to read all characters given
+$string= $string."/"; 
+$counter= 0;
+while(strlen($string) >= 1) {
+  // Get the first occurance of the character
+  $compare= substr($string, 0, 1); 
+
+  // Compare our temp variable
+  if($temp == $compare) {    
+    // Increment counter
+    $counter= $counter + 1;
+    // Removed first index and return the new string
+    $string= substr($string, 1, strlen($string));
+  } 
+  else {
+    // We've reached the unequal part
+    // Load it to our array
+    $letters[]= array(
+      'letter'=> $temp,
+      'count' => $counter
+    );
+    // echo "letter: {$temp}<br>";
+    // echo "count: {$counter}<br>";
+    $temp= $compare;
+    $counter= 0;
+  }
+}
+
+// Finally display the output
+foreach($letters as $letter) {
+  echo sprintf('%s%s', $letter['letter'], $letter['count']);
+}
+
+?>
